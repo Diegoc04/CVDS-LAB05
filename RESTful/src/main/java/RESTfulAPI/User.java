76c1ecc4939b3ae2f26eb80bcd2173a83012aa17
Daniel Santiago Torres <RulosS29@outlook.es>
2024-02-23 13:19:07 -0500

    Cosita

---
+++ RESTful/src/main/java/RESTfulAPI/User.java
@@ -0,0 +1,40 @@
+package RESTfulAPI;
+
+public class User {
+    private String userId;
+    private String id;
+    private String title;
+    private boolean completed;
+
+    public String getUserId(){
+        return userId;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public boolean getCompleted(){
+        return completed;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setCompleted(){
+        this.completed = completed;
+    }
+}
---
+++ RESTful/src/test/java/RESTfulAPI/RESTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package RESTfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RESTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
